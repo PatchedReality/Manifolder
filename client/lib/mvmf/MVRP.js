b2d2024873441f3a33e9df521b2f98943ccb6f32
Patrick O'Shaughnessey <patrick@patchedreality.com>
2026-01-24 16:43:44 -0600

    Updated MVMF files

--- client/lib/mvmf/MVRP.js
+++ client/lib/mvmf/MVRP.js
@@ -11,7 +11,7 @@ require ('@metaversalcorp/mvsb');
 require ('@metaversalcorp/mvxp');
 */
 
-MV.MVRP     = MV.Library ('MVRP',     'Copyright 2023-2024 Metaversal Corporation. All rights reserved.', 'Metaversal RP1 Platform', '0.23.21');
+MV.MVRP     = MV.Library ('MVRP',     'Copyright 2023-2024 Metaversal Corporation. All rights reserved.', 'Metaversal RP1 Platform', '0.23.24');
 
 MV.MVRP.Class.FLOAT3 = class extends MV.MVMF.Class.BASE
 {
@@ -2431,9 +2431,9 @@ MV.MVRP.MSF = class extends MV.MVMF.NOTIFICATION
                   let pServices = pPrimary["services"];
                   let pRequire;
 
-                  if ((pRequire = MV.MVMF.Core.Require (pPrimary.sRequire, pPrimary.sService, pPrimary.sNamespace)) != null)
+                  if ((pRequire = MV.MVMF.Core.Require (pPrimary.require, pPrimary.service, pPrimary.namespace)) != null)
                   {
-                     this.#pLnG      = MV.MVMF.Core.LnG_Open (pPrimary.sNamespace, pPrimary.sService, pPrimary.sConnect);
+                     this.#pLnG      = MV.MVMF.Core.LnG_Open (pPrimary.namespace, pPrimary.service, pPrimary.connect);
                      this.#bLnGAlloc   = true;
                      this.#pLnG.Attach (this);
 
@@ -2441,12 +2441,12 @@ MV.MVRP.MSF = class extends MV.MVMF.NOTIFICATION
                      {
                         let pConfig = pServices[sName];
 
-                        if ((pRequire = MV.MVMF.Core.Require (pConfig.sRequire, pConfig.sService, pConfig.sNamespace)) != null)
+                        if ((pRequire = MV.MVMF.Core.Require (pConfig.require, pConfig.service, pConfig.namespace)) != null)
                         {
                            this.#apSvc[sName] = {};
                            this.#apSvc[sName].bRequired = pConfig.bRequired;
                            this.#apSvc[sName].bAuth     = pConfig.bAuth;
-                           this.#apSvc[sName].pLnG      = MV.MVMF.Core.LnG_Open (pConfig.sNamespace, pConfig.sService, pConfig.sConnect);
+                           this.#apSvc[sName].pLnG      = MV.MVMF.Core.LnG_Open (pConfig.namespace, pConfig.service, pConfig.connect);
                            this.#apSvc[sName].pLnG.Attach (this);
                         }
                      }
@@ -2463,12 +2463,17 @@ MV.MVRP.MSF = class extends MV.MVMF.NOTIFICATION
                   let pConfig = this.#pMSFConfig["map"];
                   let pRequire;
 
-                  if ((pRequire = MV.MVMF.Core.Require (pConfig.sRequire, pConfig.sService, pConfig.sNamespace)) != null)
+if (pConfig.sRequire)
+   pRequire =  MV.MVMF.Core.Require (pConfig.sRequire, pConfig.sService, pConfig.sNamespace);
+else pRequire = MV.MVMF.Core.Require (pConfig.require, pConfig.service, pConfig.namespace)
+                  if (pRequire != null)
                   {
                      this.#apSvc["map"] = {};
                      this.#apSvc["map"].bAuth       = pConfig.bAuth;
                      this.#apSvc["map"].bRequired   = true;
+if (pConfig.sRequire)
                      this.#apSvc["map"].pLnG        = MV.MVMF.Core.LnG_Open (pConfig.sNamespace, pConfig.sService, pConfig.sConnect);
+else this.#apSvc["map"].pLnG        = MV.MVMF.Core.LnG_Open (pConfig.namespace, pConfig.service, pConfig.connect);
                      this.#apSvc["map"].pLnG.Attach (this);
                   }
                }
@@ -2604,10 +2609,10 @@ MV.MVRP.MSF = class extends MV.MVMF.NOTIFICATION
       }
   }
 
-   get pLnG ()         { return this.#pLnG;                                             }
+   get pLnG ()         { return this.#pLnG;                                           }
    get twRPersonaIx () { return this.#pRUser ? this.#pRUser.twRPersonaIx_Default : 0; }
-   get XHRError ()     { return this.#XHRError;                                           }
-   get pMSFConfig ()   { return this.#pMSFConfig;                                         }
+   get XHRError ()     { return this.#XHRError;                                       }
+   get pMSF_Map ()     { return this.pMSFConfig["map"];                               }
 
    IsReady ()
    {
@@ -2621,19 +2626,11 @@ MV.MVRP.MSF = class extends MV.MVMF.NOTIFICATION
       return this.#apSvc[sName] ? this.#apSvc[sName].pLnG : '';
    }
 
+   get pMSFConfig ()   { return this.#pMSFConfig;                                         }
+
    GetMapRootUrl ()
    {
-      return (this.#pMSFConfig && this.#pMSFConfig["map"] && this.#pMSFConfig["map"].sRootUrl) ? this.#pMSFConfig["map"].sRootUrl : '';
-   }
-
-   GetMapWClass ()
-   {
-      return (this.#pMSFConfig && this.#pMSFConfig["map"]) ? this.#pMSFConfig["map"].wClass : undefined;
-   }
-
-   GetMapObjectIx ()
-   {
-      return (this.#pMSFConfig && this.#pMSFConfig["map"]) ? this.#pMSFConfig["map"].twObjectIx : undefined;
+      return (this.#pMSFConfig && this.#pMSFConfig["map"] && this.#pMSFConfig["map"].RootUrl) ? this.#pMSFConfig["map"].RootUrl : '';
    }
 }
 
